88c2b657834cec770a6fe85eb399171ba3b6ac71
Kaizan08 <jlsnyder08@gmail.com>
2017-10-25 16:04:06 -0500

    added editing functionality to movies and new create form

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -24,6 +24,8 @@ namespace Vidly2.Models
 
         public DbSet<Movie> Movies { get; set; }
 
+        public DbSet<Genre> Genres { get; set; }
+
         public DbSet<MembershipType> MembershipTypes { get; set; }
 
         public ApplicationDbContext()
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -15,14 +15,17 @@ namespace Vidly2.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Genre")]
         [Required]
         public Genre Genre { get; set; }
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         
+        [Display(Name = "Number In Stock")]
         public byte NumberInStock { get; set; }
     }
 
