e4f950af10802481067d2aab638ed2b228d44505
Kaizan08 <jlsnyder08@gmail.com>
2017-11-01 13:45:43 -0500

    Adding Dtos and understanding Automapper functionality

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace Vidly2.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
